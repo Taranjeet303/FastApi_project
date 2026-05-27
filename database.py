9efde14347fe007ec8dece5c1ec3cbe176fc5603
Taranjeet303 <taranjeetkaurtk303@gmail.com>
2026-05-27 20:02:58 +0530

    changes

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:Taranjeet303@localhost:5432/fastapi_db"
+DATABASE_URL = "postgresql://postgres:password@localhost:5432/fastapi_db"
 
 engine = create_engine(DATABASE_URL)
 
